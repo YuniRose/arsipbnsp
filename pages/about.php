bb5a46802ceb842b024abc143126b48f6c78ce9f
YuniRose <imyunirose@gmail.com>
2021-10-27 11:58:54 +0700

    bismillah

---
+++ pages/about.php
@@ -0,0 +1,16 @@
+<div id="content" class="p-4 p-md-5 pt-5">
+    <div id="label-page"><h3><b>About</b></h3></div>
+    <div>
+        <table>
+            <tr>
+                <td><img src="./img/yumeko.jpg" width=140px height=190px></td>
+                <td></td>
+                <td style="color: black">Aplikasi ini dibuat oleh:<br>
+                    Nama: Yuni Rose AD<br>
+                    NIM: 1931710160<br>
+                    Tanggal: 27 Oktober 2021
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
